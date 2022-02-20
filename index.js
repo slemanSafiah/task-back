f50a9916c75384a09475e457c8653876ed0c9af0
sleman safiah <slemansafiah43@gmail.com>
2022-02-21 01:55:01 +0200

    asd

--- index.js
+++ index.js
@@ -8,19 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse json request body
 app.use(bodyParser.json({ limit: '50mb' }));
 
-//************************** Access Origin ****************************************/
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-	next();
-});
-
-// enable cors
-app.use(cors());
-app.options('*', cors());
-
-//************************************************************
+**********************************************************
 
 app.get("/getTasks", (req, res) => {
   let ti = setTimeout(() => {
